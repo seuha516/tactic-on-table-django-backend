bb70cd3bdea1c76982925659e85af4d1b99d406c
Seungha Jeon <seuha516@naver.com>
2022-08-06 02:20:02 +0900

    [FIX] change image upload urls

--- tactic_on_table/urls.py
+++ tactic_on_table/urls.py
@@ -3,5 +3,5 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('image/', include('image.urls')),
+    path('media/', include('image.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
