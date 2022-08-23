a816f965550326da091cbd65bde0eefea40320dc
Seungha Jeon <seuha516@naver.com>
2022-08-23 22:34:35 +0900

    Update wsgi.py

--- tactic_on_table/wsgi.py
+++ tactic_on_table/wsgi.py
@@ -1,12 +1,7 @@
-import sys, os
-
-path = os.path.abspath(__file__ + '../..')
-
-if path not in sys.path:
-    sys.path.append(path)
+import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tactic_on_table.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tactic_on_table.settings.settings')
 
 application = get_wsgi_application()
