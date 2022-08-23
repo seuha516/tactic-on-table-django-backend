0d43069a32cd5c5f482175053055c1ce6a4d987e
Seungha Jeon <seuha516@naver.com>
2022-08-23 22:22:53 +0900

    [FIX] uwsgi

--- tactic_on_table/settings.py
+++ tactic_on_table/settings.py
@@ -136,11 +136,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "https://tactic-on-table.netlify.app"
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+#     "https://tactic-on-table.netlify.app"
+# ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
