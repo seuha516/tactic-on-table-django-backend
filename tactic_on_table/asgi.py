72a3b55c5431377769b6b2e4284112db5586db54
Seungha Jeon <seuha516@naver.com>
2022-08-24 19:33:11 +0900

    Update asgi.py

--- tactic_on_table/asgi.py
+++ tactic_on_table/asgi.py
@@ -1,6 +1,8 @@
-import os
-from django.core.asgi import get_asgi_application
+import os, django
+from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tactic_on_table.settings')
 
-application = get_asgi_application()
+django.setup()
+
+application = get_default_application()
