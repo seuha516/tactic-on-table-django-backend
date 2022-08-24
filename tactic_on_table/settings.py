86796ccf785549f185583deffd4ad0ca2b100759
Seungha Jeon <seuha516@naver.com>
2022-08-24 18:48:46 +0900

    [FIX] setting

--- tactic_on_table/settings.py
+++ tactic_on_table/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sslserver'
 ]
 
 ASGI_APPLICATION = 'tactic_on_table.routing.application'
